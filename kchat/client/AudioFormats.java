7f2828df2c00cb5f203cbbc43335a8c74c8ee447
yoavkedem1 <34098545+yoavkedem1@users.noreply.github.com>
2019-05-24 09:52:24 +0300

    Add files via upload

---
+++ kchat/client/AudioFormats.java
@@ -0,0 +1,7 @@
+package kchat.client;
+
+import javax.sound.sampled.AudioFormat;
+
+public class AudioFormats {
+	public static final AudioFormat _44100HZ_16BIT = new AudioFormat(44100, 16 , 1, true, true);
+}
